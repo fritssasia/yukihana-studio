6618afe105f03f361f1ec93fbb66bdfac1a622ed
Rivas Frits Sasia <fritssasia@gmail.com>
2021-11-19 14:06:48 +0800

    update: script for popup video section #works-section

---
+++ asset/js/our-works-popup-video.js
@@ -0,0 +1,43 @@
+// Our Works PopUp Video
+$(document).ready(function () {
+    var url = $(".ourWorksVideo").attr('src');
+    $(".ourWorksPopUpVideo01").on('hide.bs.modal', function () {
+        $(".ourWorksVideo").attr('src', '');
+    });
+    $(".ourWorksPopUpVideo01").on('show.bs.modal', function () {
+        $(".ourWorksVideo").attr('src', url);
+    });
+});
+
+// Our Works PopUp Video
+$(document).ready(function () {
+    var url = $(".ourWorksVideo").attr('src');
+    $(".ourWorksPopUpVideo02").on('hide.bs.modal', function () {
+        $(".ourWorksVideo").attr('src', '');
+    });
+    $(".ourWorksPopUpVideo02").on('show.bs.modal', function () {
+        $(".ourWorksVideo").attr('src', url);
+    });
+});
+
+// Our Works PopUp Video
+$(document).ready(function () {
+    var url = $(".ourWorksVideo").attr('src');
+    $(".ourWorksPopUpVideo03").on('hide.bs.modal', function () {
+        $(".ourWorksVideo").attr('src', '');
+    });
+    $(".ourWorksPopUpVideo03").on('show.bs.modal', function () {
+        $(".ourWorksVideo").attr('src', url);
+    });
+});
+
+// Our Works PopUp Video
+$(document).ready(function () {
+    var url = $(".ourWorksVideo").attr('src');
+    $(".ourWorksPopUpVideo04").on('hide.bs.modal', function () {
+        $(".ourWorksVideo").attr('src', '');
+    });
+    $(".ourWorksPopUpVideo04").on('show.bs.modal', function () {
+        $(".ourWorksVideo").attr('src', url);
+    });
+});

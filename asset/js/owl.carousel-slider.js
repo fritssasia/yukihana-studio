d2b44893271c4aaaef4ad7f70f574be1f38e6a31
Rivas Frits Sasia <fritssasia@gmail.com>
2021-11-21 06:16:21 +0800

    update: font, css, responsive, fix slider

---
+++ asset/js/owl.carousel-slider.js
@@ -0,0 +1,46 @@
+// Slider : Our Works
+var sliderOurWorks = $('#sliderOurWork');
+sliderOurWorks.owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        }
+    }
+});
+
+// Slider : Pricing
+var sliderPricing = $('#sliderPricing');
+sliderPricing.owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 4
+        }
+    }
+});
+
+// Custom Owl Carousel Navigation Button
+$(".btn-slider-pricing-prev").click(function () {
+    sliderPricing.trigger('prev.owl.carousel');
+});
+$(".btn-slider-pricing-next").click(function () {
+    sliderPricing.trigger('next.owl.carousel');
+});
+
+$(".btn-slider-our-works-prev").click(function () {
+    sliderOurWorks.trigger('prev.owl.carousel');
+});
+$(".btn-slider-our-works-next").click(function () {
+    sliderOurWorks.trigger('next.owl.carousel');
+});
--- asset/js/popup-video.js
+++ asset/js/popup-video.js
@@ -1,4 +1,15 @@
-// Our Works PopUp Video
+// Hero Section : Video
+$(document).ready(function () {
+    var url = $(".heroSectionVideo").attr('src');
+    $(".heroSectionPopUpVideo").on('hide.bs.modal', function () {
+        $(".heroSectionVideo").attr('src', '');
+    });
+    $(".heroSectionPopUpVideo").on('show.bs.modal', function () {
+        $(".heroSectionVideo").attr('src', url);
+    });
+});
+
+// Our Works : PopUp Video 01
 $(document).ready(function () {
     var url = $(".ourWorksVideo").attr('src');
     $(".ourWorksPopUpVideo01").on('hide.bs.modal', function () {
@@ -9,7 +20,7 @@ $(document).ready(function () {
     });
 });
 
-// Our Works PopUp Video
+// Our Works : PopUp Video 02
 $(document).ready(function () {
     var url = $(".ourWorksVideo").attr('src');
     $(".ourWorksPopUpVideo02").on('hide.bs.modal', function () {
@@ -20,7 +31,7 @@ $(document).ready(function () {
     });
 });
 
-// Our Works PopUp Video
+// Our Works : PopUp Video 03
 $(document).ready(function () {
     var url = $(".ourWorksVideo").attr('src');
     $(".ourWorksPopUpVideo03").on('hide.bs.modal', function () {
@@ -31,7 +42,7 @@ $(document).ready(function () {
     });
 });
 
-// Our Works PopUp Video
+// Our Works : PopUp Video 04
 $(document).ready(function () {
     var url = $(".ourWorksVideo").attr('src');
     $(".ourWorksPopUpVideo04").on('hide.bs.modal', function () {

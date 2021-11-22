f078b7440dcc802bcaf8ae72ced56a2872af0e30
Rivas Frits Sasia <fritssasia@gmail.com>
2021-11-22 17:45:12 +0800

    update slider

--- asset/js/owl.carousel-slider.js
+++ asset/js/owl.carousel-slider.js
@@ -1,6 +1,6 @@
-// Slider : Our Works
-var sliderOurWorks = $('#sliderOurWork');
-sliderOurWorks.owlCarousel({
+// Slider - Our Works - Music Video : Start
+var sliderOurWorks = $('.sliderOurWorks')
+$('#sliderOurWorksMusicVideo').owlCarousel({
     loop: true,
     margin: 10,
     nav: false,
@@ -13,6 +13,32 @@ sliderOurWorks.owlCarousel({
         }
     }
 });
+// Slider - Our Works - Music Video : End
+
+// Slider - Our Works - Preview Video : Start
+$('#sliderOurWorksPreviewVideo').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        }
+    }
+});
+// Slider - Our Works - Preview Video : Start
+
+// Slider - Our Works - Custom Navigation Button : Start
+$(".btn-slider-our-works-prev").click(function () {
+    sliderOurWorks.trigger('prev.owl.carousel');
+});
+$(".btn-slider-our-works-next").click(function () {
+    sliderOurWorks.trigger('next.owl.carousel');
+});
+// Slider - Our Works - Custom Navigation Button : End
 
 // Slider : Pricing
 var sliderPricing = $('#sliderPricing');
@@ -36,17 +62,9 @@ sliderPricing.owlCarousel({
     }
 });
 
-// Custom Owl Carousel Navigation Button
 $(".btn-slider-pricing-prev").click(function () {
     sliderPricing.trigger('prev.owl.carousel');
 });
 $(".btn-slider-pricing-next").click(function () {
     sliderPricing.trigger('next.owl.carousel');
-});
-
-$(".btn-slider-our-works-prev").click(function () {
-    sliderOurWorks.trigger('prev.owl.carousel');
-});
-$(".btn-slider-our-works-next").click(function () {
-    sliderOurWorks.trigger('next.owl.carousel');
 });

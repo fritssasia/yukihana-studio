6d68e2929e5602aca9d4e198b15077bb33caf2be
Rivas Frits Sasia <fritssasia@gmail.com>
2021-11-22 18:33:31 +0800

    update: slider section #works-section and #pricing-section

--- asset/js/owl.carousel-slider.js
+++ asset/js/owl.carousel-slider.js
@@ -1,46 +1,4 @@
-// Slider - Our Works - Music Video : Start
-var sliderOurWorks = $('.sliderOurWorks')
-$('#sliderOurWorksMusicVideo').owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: false,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 2
-        }
-    }
-});
-// Slider - Our Works - Music Video : End
-
-// Slider - Our Works - Preview Video : Start
-$('#sliderOurWorksPreviewVideo').owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: false,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 2
-        }
-    }
-});
-// Slider - Our Works - Preview Video : Start
-
-// Slider - Our Works - Custom Navigation Button : Start
-$(".btn-slider-our-works-prev").click(function () {
-    sliderOurWorks.trigger('prev.owl.carousel');
-});
-$(".btn-slider-our-works-next").click(function () {
-    sliderOurWorks.trigger('next.owl.carousel');
-});
-// Slider - Our Works - Custom Navigation Button : End
-
-// Slider : Pricing
+// == Slider - Pricing : Start ==
 var sliderPricing = $('#sliderPricing');
 sliderPricing.owlCarousel({
     loop: true,
@@ -62,9 +20,39 @@ sliderPricing.owlCarousel({
     }
 });
 
+// Slider - Pricing - Custom Navigation Button
 $(".btn-slider-pricing-prev").click(function () {
     sliderPricing.trigger('prev.owl.carousel');
 });
 $(".btn-slider-pricing-next").click(function () {
     sliderPricing.trigger('next.owl.carousel');
 });
+// == Slider - Pricing : End ==
+
+// == Slider - Our Works : Start ==
+var allSliderOurWorks = $("#sliderOurWorksMusicVideo, #sliderOurWorksPreviewVideo, #sliderOurWorksIntroOutro, #sliderOurWorksStringer");
+
+allSliderOurWorks.each(function () {
+    $(this).owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            }
+        }
+    });
+});
+
+// Slider - Our Works - Custom Navigation Button
+$(".btn-slider-our-works-prev").click(function () {
+    allSliderOurWorks.trigger('prev.owl.carousel');
+});
+$(".btn-slider-our-works-next").click(function () {
+    allSliderOurWorks.trigger('next.owl.carousel');
+});
+// == Slider - Our Works : End ==
